7328349caf4c78c0a724eff753b057da80288ada
Arthur FEREY <arthur.ferey@acensi.fr>
2024-01-03 15:07:54 +0100

    initialisation

---
+++ src/main/java/com/back_spring_boot_book/BackSpringBootBookApplication.java
@@ -0,0 +1,13 @@
+package com.back_spring_boot_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSpringBootBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSpringBootBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back_spring_boot_book/BackSpringBootBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back_spring_boot_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSpringBootBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
